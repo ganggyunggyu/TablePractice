fea6c46e5e5d6996f38156f98f6c0ebd46b56d38
강경규 <qwzx16@naver.com>
2023-12-04 02:19:43 +0900

    FEAT: 삭제 로직 변경
    - 버튼을 클릭하면 실행됩니다
    - 기존에는 useEffect를 통해 상태관리를 했으나 버튼에 직접 함수를 주어서
    - 어떤 기능인지 더 확실하게 볼수 있게 함
    - 하위 컴포넌트에서 상위 컴포넌트의 함수 실행을 했는지에 대한 정보가 필요했는데
    - 필요 없어졌음

--- src/components/TableItem.jsx
+++ src/components/TableItem.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Thead from './Thead';
 import Tbody from './Tbody';
@@ -9,22 +9,81 @@ export default function TableItem({ title }) {
       이수: '전공',
       필수: '필수',
       과목명: '',
-      학점: 0,
+      학점: '',
       출석점수: 0,
       과제점수: 0,
       중간고사: 0,
       기말고사: 0,
       총점: 0,
+      평균: 0,
       성적: '',
     },
   ]);
-  const [isCalc, setIsCalc] = useState(false);
-  const [isDelete, setIsDelete] = useState(false);
-
   const [result, setResult] = useState([0, 0, 0, 0, 0, 0, 0, '']);
 
+  const get성적 = (총점) => {
+    if (총점 >= 95) return 'A+';
+    if (총점 >= 90) return 'A';
+    if (총점 >= 85) return 'B+';
+    if (총점 >= 80) return 'B0';
+    if (총점 >= 75) return 'C+';
+    if (총점 >= 70) return 'C0+';
+    if (총점 >= 65) return 'D+';
+    if (총점 >= 60) return 'D0';
+    if (총점 < 60) return 'F';
+  };
+
+  const resultCalc = (tbodyInfos) => {
+    const copyResult = [];
+    let 총학점 = 0;
+    let 총출석점수 = 0;
+    let 총과제점수 = 0;
+    let 총중간고사 = 0;
+    let 총기말고사 = 0;
+    let 총총점 = 0;
+    let cnt = tbodyInfos.length;
+    for (const tbodyInfo of tbodyInfos) {
+      if (tbodyInfo.학점 === 1) {
+        cnt = cnt - 1;
+        continue;
+      } else {
+        총학점 = 총학점 + +tbodyInfo.학점;
+        총출석점수 = 총출석점수 + +tbodyInfo.출석점수;
+        총과제점수 = 총과제점수 + +tbodyInfo.과제점수;
+        총중간고사 = 총중간고사 + +tbodyInfo.중간고사;
+        총기말고사 = 총기말고사 + +tbodyInfo.기말고사;
+        총총점 = 총총점 + +tbodyInfo.총점;
+        console.log(tbodyInfo.총점);
+      }
+    }
+    copyResult.push(총학점);
+    copyResult.push(총출석점수);
+    copyResult.push(총과제점수);
+    copyResult.push(총중간고사);
+    copyResult.push(총기말고사);
+    copyResult.push(+총총점);
+    copyResult.push(+(총총점 / cnt).toFixed(1));
+    copyResult.push(get성적(+총총점 / cnt));
+    console.log(cnt);
+    return copyResult;
+  };
+
   const 저장 = () => {
-    setIsCalc(!isCalc);
+    const copyResult = resultCalc(tbodyInfos);
+    console.log(copyResult);
+    setResult(copyResult);
+    const copyTbodyInfos = [...tbodyInfos];
+    copyTbodyInfos.sort((a, b) => {
+      if (a.이수 === b.이수) {
+        if (a.필수 === b.필수) {
+          return a.과목명.localeCompare(b.과목명);
+        }
+        return a.필수.localeCompare(b.필수);
+      }
+      return a.이수.localeCompare(b.이수);
+    });
+
+    setTbodyInfos(copyTbodyInfos);
   };
 
   const 추가 = () => {
@@ -33,53 +92,56 @@ export default function TableItem({ title }) {
       이수: '전공',
       필수: '필수',
       과목명: '',
-      학점: 0,
+      학점: '',
       출석점수: 0,
       과제점수: 0,
       중간고사: 0,
       기말고사: 0,
       총점: 0,
+      평균: 0,
       성적: '',
     };
     copyTbodys.push(tbody);
-
     setTbodyInfos(copyTbodys);
   };
 
   const 삭제 = () => {
-    const copyTbody = [...tbodyInfos];
-    setTbodyInfos(copyTbody.splice(seletId, 1));
+    const copyResult = resultCalc(tbodyInfos);
+    setResult(copyResult);
+    if (tbodyInfos.length > 1) {
+      setTbodyInfos((prevTbodyInfos) => {
+        const copyTbodys = [...prevTbodyInfos];
+        copyTbodys.splice(seletId, 1);
+        return copyTbodys;
+      });
+    } else {
+      console.log('삭제실패');
+    }
   };
-
-  // useEffect(() => {
-  //   const arr = [...tbodyInfos];
-  //   const updatedArr = arr.filter((item) => item.id !== +seletId);
-  //
-  //   setTbodyInfos([]);
-  //   setTbodyInfos(updatedArr);
-  // }, [isDelete]);
-
-  // useEffect(() => {
-  //   const result = [...tbodyInfos].sort((a, b) => {
-  //     return a.이수.localeCompare(b.이수);
-  //   });
-  //   setTbodyInfos(result);
-  // }, [isCalc]);
-
-  useEffect(() => {}, [seletId]);
-  useEffect(() => {
-    const copyTbody = [...tbodyInfos];
-    setTbodyInfos(copyTbody.slice(seletId, 1));
-  }, [isDelete]);
 
   return (
     <div key={title} className='flex flex-col gap-4'>
       <div className='flex w-full justify-between'>
-        <h1 className='text-xl font-bold'>{title}</h1>
-        <div>
-          <button onClick={추가}>추가</button>
-          <button onClick={삭제}>삭제</button>
-          <button onClick={저장}>저장</button>
+        <h1 className='text-2xl ml-3 font-bold'>{title}</h1>
+        <div className='flex gap-4'>
+          <button
+            className='bg-violet-600 text-white w-12 p-2 rounded-md hover:scale-110 transition-all'
+            onClick={추가}
+          >
+            추가
+          </button>
+          <button
+            className='bg-violet-600 text-white w-12 p-2 rounded-md hover:scale-110 transition-all'
+            onClick={저장}
+          >
+            저장
+          </button>
+          <button
+            className='bg-red-600 text-white w-12 p-2 rounded-md hover:scale-110 transition-all'
+            onClick={삭제}
+          >
+            삭제
+          </button>
         </div>
       </div>
       <table className='w-full'>
@@ -95,13 +157,12 @@ export default function TableItem({ title }) {
                 setTbodyInfos={setTbodyInfos}
                 seletId={seletId}
                 setSeletId={setSeletId}
-                isCalc={isCalc}
                 setResult={setResult}
               />
             )
           );
         })}
-        <tbody>
+        <tbody className='bg-violet-300'>
           <tr className='text-center'>
             <td colSpan={3} className='border p-1'>
               합계
